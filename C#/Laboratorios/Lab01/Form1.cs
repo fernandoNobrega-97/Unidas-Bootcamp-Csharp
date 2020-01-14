534ed233416ebf0e57791f96b901e653d07b24ef
fernandoNobrega-97 <fernandoNobrega1981@outlook.com>
2020-01-14 13:57:17 -0300

    Conclusão lab01 C#

---
+++ C#/Laboratorios/Lab01/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+{
+    public partial class lab01Form : Form
+    {
+        public lab01Form()
+        {
+            InitializeComponent();
+        }
+
+        private void calcularbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = 0, quadrado = 0;
+
+                numero = Convert.ToInt32(valorTextBox.Text);
+
+                quadrado = numero * numero;
+
+                resultadoLabel.Text = $"Quadrado de {numero} é: {quadrado.ToString("N0")}";
+               
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Favor digitar apenas número.");
+            }
+            catch(OverflowException)
+            {
+                MessageBox.Show("Número informado extenso demais para cálculo.");
+            }
+            finally
+            {
+                valorTextBox.Focus();
+                valorTextBox.SelectAll();
+            }
+        }
+
+        private void lab01Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
